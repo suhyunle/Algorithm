8097e9a684a48da9003c89e0e0a340e4b81e4059
Suhyun Lee <122947409+suhyunle@users.noreply.github.com>
2025-02-28 10:26:02 +0900

    Update 1이될때까지.py

--- 그리디/문제/1이될때까지.py
+++ 그리디/문제/1이될때까지.py
@@ -10,3 +10,52 @@ N을 1로 만드는 최소 횟수이다.
 
 N과 K 가 주어질때 N이 1이 될때까지 1번 혹은 2번의 과정을 수행해야하는 최소 횟수를 구하는 프로그램을 작성하시오. 
 """
+#1.
+n,k = map(int, input().split())
+result =0 
+
+while n>= k :
+  #N이 K로 나누어 떨어지지 않는다면 N에서 1씩 빼기
+  while n%k !=0:
+    n-=1
+    result +=1
+
+#마지막으로 남은 수에 대하여 1씩 빼기
+
+while n>1:
+  n-=1
+  result +=1
+
+print(result)
+
+#2. 
+
+N, K를 공백으로 하여 입력받기. 
+n,k =map(int,input().split())
+result =0
+
+while True:
+  #(N==K로 나누어 떨어지는 수 )가 될때까지 1씩 빼기 .
+  target = (n//k)*k
+  
+"""
+n // k : n을 k로 나눈 몫을 구함 (소수점 버림)
+* k : 그 몫을 다시 k로 곱해서 k의 배수를 만듦
+즉, target은 n보다 작거나 같은 가장 가까운 k의 배수가 됨.
+"""
+
+  result += (n-target)
+  n= target
+
+# N이 K보다 작을때(더이상 나눌 수 없을 때 반복문 탈출)
+if n<k:
+  break
+
+#k로 나누기
+result += 1
+n//=k
+
+#마지막으로 남은수에 대하여 1씩 빼기 
+
+result+=(n-1)
+print(result)
