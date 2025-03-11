2de191b7aeee5b9a5d6f2ae73688caabced51190
Suhyun Lee <122947409+suhyunle@users.noreply.github.com>
2025-03-11 17:17:24 +0900

    Update 상하좌우.py

--- 구현/상하좌우.py
+++ 구현/상하좌우.py
@@ -13,4 +13,23 @@ x,y=1,1
 plans = input().split()
 
 #L,R,U,D에 따른 이동방향. 
-move_
+move_types =['L','R','U','D']
+
+
+#이동 계획을 하나씩 확인
+for plan in plans: 
+  if plan == move_types[i]:
+    nx=x+dx[i]
+    ny=y+dy[i]
+    
+  #이동후 좌표 구하기 
+
+
+#공간을 벗어나는 경우 무시
+if nx<1 or my <1 or nx>n or ny>n:
+  continue
+  #이동수행
+  x,y =nx,ny
+
+
+print(x,y)
